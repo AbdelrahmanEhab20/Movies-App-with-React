48d62cb60a7b2aa0a95b6846484c17eabb428355
AbdelrahmanEhab <57192912+AbdelrahmanEhab20@users.noreply.github.com>
2022-02-02 13:14:31 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import React, { useState } from "react";
+import Login from "./components/login/login";
+import FormComponent from "./components/register/register";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import NavBar from "./components/navbar/navbar";
+import OneMovie from "./components/movies/movies";
+import Details from "./components/details/detail";
+import notfound from './components/notfound/notfound';
+import Specific from './components/specificDetail/specific';
+import favorite from './components/favorite/favorite'
+import { LanguageContext } from "./context/languageContext";
 function App() {
+  const [contextLanguage, setContextLanguage] = useState('en-US');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <LanguageContext.Provider value={{ contextLanguage, setContextLanguage }}>
+      <BrowserRouter>
+        <NavBar />
+        <br />
+        <Switch>
+          <Route path={"/"} exact component={FormComponent}></Route>
+          <Route path={"/login"} exact component={Login}></Route>
+          <Route path={"/OneMovie/:id"} exact component={OneMovie}></Route>
+          <Route path={"/details"} exact component={Details}></Route>
+          <Route path={"/Specific"} exact component={Specific}></Route>
+          <Route path={"/favorites"} exact component={favorite}></Route>
+          <Route path={"*"} exact component={notfound}></Route>
+        </Switch>
+      </BrowserRouter>
+    </LanguageContext.Provider>
   );
 }
 
